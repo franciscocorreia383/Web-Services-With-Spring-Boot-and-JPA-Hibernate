20b2d9e6d4392c8a47b9dcf6f7c70f4fae6de029
Francisco Correia <franciscocorreia383@gmail.com>
2022-04-08 18:22:14 -0300

    Create atributes getSubTotal() on OrderItem and getTotal() on Order

--- src/main/java/com/example/demo/entities/OrderItem.java
+++ src/main/java/com/example/demo/entities/OrderItem.java
@@ -68,6 +68,10 @@ public class OrderItem implements Serializable{
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
 
 	@Override
 	public int hashCode() {
