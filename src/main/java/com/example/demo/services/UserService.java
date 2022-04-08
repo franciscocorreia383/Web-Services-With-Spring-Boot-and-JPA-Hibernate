efc6b5000552eee2d0358af429589e4b6ecf8263
Francisco Correia <franciscocorreia383@gmail.com>
2022-04-08 19:44:36 -0300

    Create User register and created reponse on RestController

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		Optional<User> object = repository.findById(id);
 		return object.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 }
